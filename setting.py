7328deec403d7eaa68c262acf1c03ced8a6044b9
thangquang09 <lythang09092004@gmail.com>
2025-04-05 10:29:38 +0700

    feat: add settings module to store application constants

---
+++ setting.py
@@ -0,0 +1,10 @@
+"""setting.py
+
+Description: Store constants and settings for the application.
+"""
+
+MAX_HISTORY = 10 # maximum number of chat history
+USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+CHUNK_SIZE = 512
+CHUNK_OVERLAP = 50
+K = 5
