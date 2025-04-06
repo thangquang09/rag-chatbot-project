214db498bd06fbb0459068cda62ef6ce5bc6c641
thangquang09 <lythang09092004@gmail.com>
2025-04-06 13:37:54 +0700

    feat: update K constant to improve model performance

--- setting.py
+++ setting.py
@@ -7,6 +7,6 @@ MAX_HISTORY = 10  # maximum number of chat history
 USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
 CHUNK_SIZE = 512
 CHUNK_OVERLAP = 50
-K = 5
+K = 9
 MAX_TOKENS_INPUT = 200
 MAX_TOKENS_DOCS = 1000
